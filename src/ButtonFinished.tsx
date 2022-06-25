f15b85068f8748b3e2fc11e9eb60e2816cbba265
abimaelst <abimaest@gmail.com>
2022-06-25 16:36:50 -0300

    Feat: create button todo

---
+++ src/ButtonFinished.tsx
@@ -0,0 +1,8 @@
+import { Check } from "phosphor-react";
+export function ButtonTask() {
+  return (
+    <button className="w-10 h-5 border rounded-full flex items-center justify-center bg-purple-500">
+      <Check size={12} />
+    </button>
+  );
+}
