db81c6c1d11393d49ec3b3459d05a595a6db6904
abimaelst <abimaest@gmail.com>
2022-06-25 16:37:07 -0300

    Feat: create button task finished

---
+++ src/ButtonTodo.tsx
@@ -0,0 +1,6 @@
+import { Check } from "phosphor-react";
+export function ButtonTodo() {
+  return (
+    <button className="w-10 h-5 border rounded-full flex items-center justify-center bg-transparent border-sky-400"></button>
+  );
+}
